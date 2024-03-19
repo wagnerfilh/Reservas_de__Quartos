367963fb0e7619588eececbbe686c8a28a981ad4
Wagner Filho <81321824+wagnerfilh@users.noreply.github.com>
2024-03-19 16:01:31 -0300

    Update ClienteReservas.java
    Errei no texto das instruções sobre os as ações das reservas.

--- Hotel/ClienteReservas.java
+++ Hotel/ClienteReservas.java
@@ -20,7 +20,7 @@ public class ClienteReservas {
                     System.out.println("Digite um número válido"); 
             }do{
             System.out.println("Vamos lá! o que podemos fazer por você hoje?");
-            System.out.println("Digite 1 para fazer uma reserva, 2 para fazer uma nova reserva, 3 para modificar uma reserva, 4 para cancelar reservas ou -1 para sair:");
+            System.out.println("Digite 1 para fazer uma reserva, 2 para consultar uma reserva, 3 para modificar uma reserva, 4 para cancelar reservas ou -1 para sair:");
             switch (numero) {
                 case 1:
                     String result = cli.cliente(numero);
