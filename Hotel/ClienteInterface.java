4ddcbf96555e0a824a1b433116b4c4db8e5a0069
Wagner Filho <81321824+wagnerfilh@users.noreply.github.com>
2024-03-20 23:44:11 -0300

    Coloquei o diretório erradokkkkkkkkkk

--- Hotel/ClienteInterface.java
+++
@@ -1,7 +0,0 @@
-package Hotel;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClienteInterface extends Remote {
-    String cliente(int requerimento) throws RemoteException;
-}
